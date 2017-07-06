43b027339bdb20f26b77b007e8f1c22a58ab9770
Yimeng Zhang <zym1010@gmail.com>
2017-07-05 23:18:10 -0400

    finish stereo toolbox

--- add_dependencies.sh
+++ add_dependencies.sh
@@ -2,6 +2,7 @@
 
 conda install --yes caffe gflags=2.1.2
 conda install --yes --no-update-dependencies -c conda-forge scikit-learn joblib scikit-image h5py
+pip install transforms3d
 # unneeded right now, as it causes some problem for installing dependencies on 06/30/2017.
 # pip install imagen
 # TODO: add spams if using Python 2.7
